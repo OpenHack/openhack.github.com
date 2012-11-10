835e582f65cdd9dca8dff9dfd2f5205ed01a5567
1000 Needles <james.childress@me.com>
2012-11-10 09:21:24 -0500

    dropdown menu is revealed when javascript is disabled

--- javascripts/city_dropdown.js
+++ javascripts/city_dropdown.js
@@ -5,7 +5,10 @@ $(function(){
 	class_name = "show_menu";	
 	wrapper = $("#menu_wrapper");
 	header = wrapper.find("#menu_header");
-		
+	menu = wrapper.find("#menu");
+	
+	menu.addClass("js");
+	
 	header.click(function(){
 		wrapper.toggleClass(class_name);
 	})
