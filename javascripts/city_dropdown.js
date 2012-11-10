67b15e58b25ad6f6628b0e83968cf58e8cc80aad
1000 Needles <james.childress@me.com>
2012-11-10 07:53:22 -0500

    animated city dropdown for tablet/phone layouts

--- javascripts/city_dropdown.js
+++ javascripts/city_dropdown.js
@@ -1,12 +1,23 @@
 $(function(){
+
+	var tablet_width, class_name, wrapper, menu, header;
+
+	tablet_width = 767;
+	class_name = "show_menu";
 	
-	var wrapper, menu, header;
 	wrapper = $("#menu_wrapper");
 	menu = wrapper.find("#menu");
 	header = wrapper.find("#menu_header");
 	
 	header.click(function(){
-		wrapper.toggleClass("show_menu");
+		if ($(window).width() <= tablet_width) {
+			wrapper.stop().toggleClass(class_name);
+			if (wrapper.hasClass(class_name)) {
+				menu.slideDown();
+			} else {
+				menu.slideUp();
+			}
+		}
 	})
 	
 });
